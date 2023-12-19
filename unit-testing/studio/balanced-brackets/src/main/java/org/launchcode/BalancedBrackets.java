b2b3526009a9a7ea0b8456059e7911b37ab2c788
Matt Speaks <mattspeaks@hotmail.com>
2023-12-18 19:58:54 -0600

    Unit Testing Studio Balanced Brackets

--- unit-testing/studio/balanced-brackets/src/main/java/org/launchcode/BalancedBrackets.java
+++ unit-testing/studio/balanced-brackets/src/main/java/org/launchcode/BalancedBrackets.java
@@ -19,13 +19,24 @@ public class BalancedBrackets {
      * @param str - to be validated
      * @return true if balanced, false otherwise
      */
+
+
     public static boolean hasBalancedBrackets(String str) {
         int brackets = 0;
+        if (str == "") {
+            return false;
+        }
+        if (!(str.contains("[")) || !(str.contains("]"))){
+            return false;
+        }
         for (char ch : str.toCharArray()) {
             if (ch == '[') {
                 brackets++;
             } else if (ch == ']') {
                 brackets--;
+                if (brackets < 0) {
+                    return false;
+                }
             }
         }
         return brackets == 0;
--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -7,7 +7,74 @@ import static org.junit.jupiter.api.Assertions.*;
 class BalancedBracketsTest {
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void balancedReturnsTrue() {
+        String str = "[]";
+        assertTrue(BalancedBrackets.hasBalancedBrackets(str));
+    }
+
+    @Test
+    public void notBalanceReturnsFalse() {
+        String str = "[][";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(str));
+    }
+
+    @Test
+    public void balanceGoesNegative() {
+        String str = "][";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(str));
+    }
+
+    @Test
+    public void stringIsEmpty() {
+        String str = "";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(str));
+    }
+
+    @Test
+    public void stringContainsNoBrackets() {
+        String str = "Hello";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(str));
+    }
+
+    @Test
+    public void stringCOntainsLettersAndBrackets() {
+        String str = "Hello[]";
+        assertTrue(BalancedBrackets.hasBalancedBrackets(str));
+    }
+
+    @Test
+    public void stringContainsBracketsAndWordBetween() {
+        String str = "[Hello]";
+        assertTrue(BalancedBrackets.hasBalancedBrackets(str));
+    }
+
+    @Test
+    public void stringContainsMultipleBrackets() {
+        String str = "[[Hello]]";
+        assertTrue(BalancedBrackets.hasBalancedBrackets(str));
+    }
+
+    @Test
+    public void stringContainsUnbalancedBrackets() {
+        String str = "[[Hello][";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(str));
+    }
+
+    @Test
+    public void stringOneRightBracket() {
+        String str = "[";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(str));
+    }
+
+    @Test
+    public void stringOneLeftBracket() {
+        String str = "]";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(str));
+    }
+
+    @Test
+    public void stringContainsBalancedButNonOrdered(){
+        String str = "[]][]][[";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(str));
     }
 }
