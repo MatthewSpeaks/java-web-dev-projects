3a98da54778b9c71ab37fa94b1c29d2bc1c44db4
Matt Speaks <mattspeaks@hotmail.com>
2024-01-27 16:53:11 -0600

    Inheritance Exercises

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Ingredient.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Ingredient.java
@@ -39,7 +39,7 @@ public abstract class Ingredient {
 
     @Override
     public String toString() {
-        return "Name: " + name + "\n" +
+        return System.lineSeparator() +"Name: " + name + "\n" +
                 "Cost: $" + cost + "\n" +
                 "Allergens: " + allergens + "\n";
     }
--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -1,6 +1,7 @@
 package org.launchcode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,9 +10,15 @@ public class Main {
         ArrayList<Cone> cones = menu.getCones();
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
-
+        Comparator comparator = new FlavorComparator();
+        Comparator comparator1 = new ConeComparator();
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
-
+        flavors.sort(comparator);
+        cones.sort(comparator1);
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        System.out.println("--------------Flavors Sorted-----------------");
+        System.out.println(flavors.toString());
+        System.out.println("--------------Cones Sorted-------------------");
+        System.out.println(cones.toString());
     }
 }
